626cc1ee10dd7759056eef17fbab83bf6e8752ba
shawnzamperini <shawnzamperini@gmail.com>
2024-07-29 13:50:31 -0700

    Adding some dependency making files

---
+++ install-deps/build-opts.sh
@@ -0,0 +1,11 @@
+# Generated automatically! Do not edit
+
+# Installation directory
+FLANSOFT=/home/zamp/flansoft
+# Various compilers
+CC=gcc
+CXX=g++
+MPICC=/home/zamp/flansoft/openmpi/bin/mpicc
+MPICXX=/home/zamp/flansoft/openmpi/bin/mpicxx
+
+Boost_INCLUDE_DIR=/home/zamp/flansoft/boost_1_85_0
---
+++ machines/mkdeps.linux.sh
@@ -0,0 +1,24 @@
+# If we are in machines directory, go up a directory
+if [ `dirname "$0"` == "." ] 
+  then
+    cd ..
+fi
+
+# flansoft is where all the dependencies will live
+export FLANSOFT=$HOME/flansoft
+mkdir -p $FLANSOFT
+
+export MACHINE_NAME='linux'
+cd install-deps
+
+# 1) Build OpenMPI.
+# ./mkdeps.sh CC=clang CXX=clang++ --build-openmpi=yes --build-luajit=no  --prefix=$GKYLSOFT
+
+# 2) Build gkylzero and adios. MacOS can use clang++, but I'm using g++ instead to be consistent.
+#./mkdeps.sh CXX=g++ MPICXX=$FLANSOFT/openmpi-4.0.5/bin/mpicxx --prefix=$FLANSOFT --build-adios=yes --build-openmpi=no
+
+# 1) Build boost first since it's needed for msgpack
+./mkdeps.sh CC=gcc CXX=g++ --prefix=$FLANSOFT --build-adios2=no --build-boost=yes --build-msgpack=no
+
+# 2) Then msgpack now that we have boost and can tell it where to look
+./mkdeps.sh CC=gcc CXX=g++ --prefix=$FLANSOFT --build-adios2=no --build-boost=no --build-msgpack=yes --new-build-opts=no
