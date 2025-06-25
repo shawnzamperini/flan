2af2e96140ef52215795eb936fe980f637768f8b
SDSUSam <sandersen1331@sdsu.edu>
2025-06-25 10:29:32 -0700

    Updated conf.py authorship
    Template used a placeholder name - I updated this with Shawn's last name and changed nothing else.

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Flan'
+copyright = '2025, Zamperini'
+author = 'Zamperini'
 
 release = '0.1'
 version = '0.1.0'
